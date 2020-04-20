ffc3d0fe8e22738d405cf84ba61f201bb9a4f26f
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-20 16:35:24 -0700

    post

--- agwebberley/agwebberleyapp/urls.py
+++ agwebberley/agwebberleyapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-	#path("", views.home, name="home"),
-	path('', HomeView.as_view(), name="home")
+
+	path('', HomeView.as_view(), name="home",)
+	path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail",)
 ]
