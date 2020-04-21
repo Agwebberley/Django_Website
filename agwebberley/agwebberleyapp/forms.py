028d0e8b0c54ded03ef14ed33a455b0bf70f6a47
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-21 10:30:49 -0700

    form

--- agwebberley/agwebberleyapp/forms.py
+++ agwebberley/agwebberleyapp/forms.py
@@ -7,7 +7,14 @@ class PostForm(forms.ModelForm):
 		fields = ('title', 'author', 'body', 'download1n', 'download1', 'download2n', 'download2')
 
 		widgets = {
-			'title': form.TextInput
+			'title': form.TextInput(attrs={'class': 'form-control'}),
+			'author': form.TextInput(attrs={'class': 'form-control'}),
+			'body': form.TextInput(attrs={'class': 'form-control'}),
+			'download1n': form.TextInput(attrs={'class': 'form-control'}),
+			'download1': form.TextInput(attrs={'class': 'form-control'}),
+			'download2n': form.TextInput(attrs={'class': 'form-control'}),
+			'download2': form.TextInput(attrs={'class': 'form-control'}),
+
 
 
 		}
