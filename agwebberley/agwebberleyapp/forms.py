a90ff0e03f39b862821e53ee8720b8ea10241f0b
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-21 11:02:43 -0700

    update

--- agwebberley/agwebberleyapp/forms.py
+++ agwebberley/agwebberleyapp/forms.py
@@ -1,4 +1,4 @@
-from django import fourms
+from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
@@ -7,13 +7,13 @@ class PostForm(forms.ModelForm):
 		fields = ('title', 'author', 'body', 'download1n', 'download1', 'download2n', 'download2')
 
 		widgets = {
-			'title': form.TextInput(attrs={'class': 'form-control'}),
-			'author': form.TextInput(attrs={'class': 'form-control'}),
-			'body': form.TextInput(attrs={'class': 'form-control'}),
-			'download1n': form.TextInput(attrs={'class': 'form-control'}),
-			'download1': form.TextInput(attrs={'class': 'form-control'}),
-			'download2n': form.TextInput(attrs={'class': 'form-control'}),
-			'download2': form.TextInput(attrs={'class': 'form-control'}),
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			'author': forms.Select(attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'download1n': forms.TextInput(attrs={'class': 'form-control'}),
+			'download1': forms.TextInput(attrs={'class': 'form-control'}),
+			'download2n': forms.TextInput(attrs={'class': 'form-control'}),
+			'download2': forms.TextInput(attrs={'class': 'form-control'}),
 
 
 
