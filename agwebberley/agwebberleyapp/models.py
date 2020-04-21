3f59ce6aad37aab36ae39110f04276319de9166b
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-21 09:15:45 -0700

    models

--- agwebberley/agwebberleyapp/models.py
+++ agwebberley/agwebberleyapp/models.py
@@ -6,6 +6,10 @@ class Post(models.Model):
 	title = models.CharField(max_length=255)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = models.TextField()
+	download1n = models.TextField(default="Download1 Name}")
+	download1 = models.TextField(default="Download1 Link")
+	download2n = models.TextField(default="Download2 Name")
+	download2 = models.TextField(default="Download2 Link")
 
 	def __str__(self):
 		return self.title + ' | ' + str(self.author)
